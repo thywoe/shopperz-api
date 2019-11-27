f821010b3a5d8c0cff396ce33e5e7ce7a974e650
thywoe <bamibabson@gmail.com>
2019-11-27 10:06:58 +0100

    add views for user

--- shopperz/api/models.py
+++ shopperz/api/models.py
@@ -2,6 +2,8 @@ from django.db import models
 
 # Create your models here.
 
+
+# This model is use to create User table in the database
 class User(models.Model):
     firstname = models.CharField(max_length=120)
     lastname = models.CharField(max_length=120)
--- shopperz/api/views.py
+++ shopperz/api/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from .models import User
+from .serializers import UserSerializer
+from rest_framework.views import APIView
 
 # Create your views here.
+
+class AllViews(APIView):
+    def get(self, request):
+        user = User.objects.all()
+        serializer = UserSerializer(user, many=True)
+        return JsonResponse(serializer.data, status=200)

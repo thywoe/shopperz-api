d3e5909c984aaac21a8d759ea10480c4e7334a2b
thywoe <bamibabson@gmail.com>
2019-11-27 09:35:24 +0100

    add user model

--- shopperz/api/models.py
+++ shopperz/api/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    firstname = models.CharField(max_length=120)
+    lastname = models.CharField(max_length=120)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=120)
+    phonenumber = models.CharField(max_length=120)
+    isShopper = models.BooleanField(default=False)
+    
+    def __str__(self):
+        return "{} - {}".format(self.firstname, self.email)
+    
